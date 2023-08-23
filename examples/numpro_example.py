4b168010a5a7b609c3d0eae20e7c505a347756a4
Brady Planden <brady.planden@gmail.com>
2023-08-23 11:30:24 +0100

    Example numpyro NUTS script

---
+++ examples/numpro_example.py
@@ -0,0 +1,119 @@
+import argparse
+import os
+
+import numpyro
+from numpyro.infer import MCMC, NUTS
+import numpyro.distributions as dist
+
+numpyro.enable_x64()
+
+import jax
+import jax.numpy as jnp
+from jax.random import PRNGKey
+from jax import pure_callback
+
+import pybamm
+import numpy as np
+
+
+def solution(t_eval, y, model, inputs):
+    termination = "final time"
+    t_event = None
+    y_event = np.array(None)
+
+    intern_sol = pybamm.Solution(
+        t_eval, y, model, inputs, t_event, y_event, termination
+    )
+    return intern_sol["Voltage [V]"].data
+
+
+def model(f, t_eval, m):
+    theta0 = numpyro.sample(
+        "theta0", dist.TruncatedNormal(loc=0.5, scale=0.1, low=0.4, high=0.6)
+    )
+    theta1 = numpyro.sample(
+        "theta1", dist.TruncatedNormal(loc=0.5, scale=0.1, low=0.4, high=0.6)
+    )
+
+    inputs = {
+        "Negative electrode active material volume fraction": theta0,
+        "Positive electrode active material volume fraction": theta1,
+    }
+
+    yhat = f(inputs=inputs)
+
+    # Attempt 1
+    V = solution(t_eval, yhat, m, inputs)
+
+    # Attempt 2
+    V = pure_callback(
+        solution, jnp.zeros(100), t_eval=t_eval, y=yhat, model=m, inputs=inputs
+    )
+
+
+def main(args):
+    # load model
+    m = pybamm.lithium_ion.SPMe()
+    m.convert_to_format = "jax"
+    m.events = []
+
+    # create geometry
+    geometry = m.default_geometry
+
+    # load parameter values and process model and geometry
+    param = m.default_parameter_values
+    param.update(
+        {
+            "Negative electrode active material volume fraction": "[input]",
+            "Positive electrode active material volume fraction": "[input]",
+        }
+    )
+    param.process_geometry(geometry)
+    param.process_model(m)
+
+    # set mesh
+    mesh = pybamm.Mesh(geometry, m.default_submesh_types, m.default_var_pts)
+
+    # discretise model
+    disc = pybamm.Discretisation(mesh, m.default_spatial_methods)
+    disc.process_model(m)
+
+    # initial solve to construct solver class
+    t_eval = np.linspace(0, 3600, 100)
+    s = pybamm.JaxSolver(atol=1e-6, rtol=1e-6, method="BDF")
+    s.solve(
+        m,
+        t_eval,
+        inputs={
+            "Negative electrode active material volume fraction": 0.5,
+            "Positive electrode active material volume fraction": 0.5,
+        },
+    )
+
+    # create solve function
+    f = s.create_solve(m, t_eval)
+
+    # run numpyro inference
+    mcmc = MCMC(
+        NUTS(model),
+        num_samples=args.num_samples,
+        num_warmup=args.num_warmup,
+        num_chains=args.num_chains,
+        progress_bar=True,
+    )
+    mcmc.run(PRNGKey(467320), f=f, t_eval=t_eval, m=m)
+    mcmc.print_summary()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="PyBaMM Model")
+    parser.add_argument("--num-samples", nargs="?", default=10, type=int)
+    parser.add_argument("--num-warmup", nargs="?", default=2, type=int)
+    parser.add_argument("--num-chains", nargs="?", default=1, type=int)
+    parser.add_argument("--device", default="cpu", type=str)
+    args = parser.parse_args()
+
+    numpyro.set_platform(args.device)
+    numpyro.set_host_device_count(args.num_chains)
+
+    main(args)
