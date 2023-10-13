ce56923797706f7d82d50765553a65feaee70ce1
Brady Planden <brady.planden@gmail.com>
2023-10-13 17:26:58 +0100

    Add prior and parameter_set tests, remove redundant catch in parameter_set

--- pybop/identification/parameter_set.py
+++ pybop/identification/parameter_set.py
@@ -8,9 +8,6 @@ class ParameterSet:
 
     def __new__(cls, method, name):
         if method.casefold() == "pybamm":
-            try:
                 return pybamm.ParameterValues(name).copy()
-            except:
-                raise ValueError("Parameter set not found")
         else:
             raise ValueError("Only PybaMM parameter sets are currently implemented")
--- tests/unit/test_parameterisation.py
+++ tests/unit/test_parameterisation.py
@@ -121,3 +121,26 @@ class TestParameterisation:
             ValueError, match="Model must be built before calling simulate"
         ):
             model.simulate(None, None)
+
+    @pytest.mark.unit
+    def test_priors(self):
+        # Tests priors
+        Gaussian = pybop.Gaussian(0.5, 1)
+        Uniform = pybop.Uniform(0, 1)
+        Exponential = pybop.Exponential(1)
+
+        np.testing.assert_allclose(Gaussian.pdf(0.5), 0.3989422804014327, atol=1e-4)
+        np.testing.assert_allclose(Uniform.pdf(0.5), 1, atol=1e-4)
+        np.testing.assert_allclose(Exponential.pdf(1), 0.36787944117144233, atol=1e-4)
+
+    @pytest.mark.unit
+    def test_parameter_set(self):
+        # Tests parameter set creation
+        with pytest.raises(ValueError):
+            pybop.ParameterSet("pybamms", "Chen2020")
+
+        parameter_test = pybop.ParameterSet("pybamm", "Chen2020")
+        np.testing.assert_allclose(
+            parameter_test["Negative electrode active material volume fraction"], 0.75
+        )
+
