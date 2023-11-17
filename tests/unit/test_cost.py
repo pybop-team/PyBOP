70025217b0d5ffe1d9f8e46516f8d59eed05a92e
NicolaCourtier <45851982+NicolaCourtier@users.noreply.github.com>
2023-11-17 17:37:15 +0000

    Revert "Remove n_parameters function check"
    This reverts commit 6d809de91e5e23f4f5fc4a184aef4751e0fbd34a.

--- tests/unit/test_cost.py
+++ tests/unit/test_cost.py
@@ -38,6 +38,8 @@ class TestCosts:
         assert cost.problem == problem
         with pytest.raises(NotImplementedError):
             cost([0.5])
+        with pytest.raises(NotImplementedError):
+            cost.n_parameters()
 
         # Root Mean Squared Error
         cost = pybop.RootMeanSquaredError(problem)
