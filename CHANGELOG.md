edf4c824c483288704c262f930330802a78db49c
NicolaCourtier <45851982+NicolaCourtier@users.noreply.github.com>
2023-12-01 12:01:26 +0000

    Refactor test_problem and update Changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,10 @@
 # [Unreleased](https://github.com/pybop-team/PyBOP)
 
 - [#116](https://github.com/pybop-team/PyBOP/issues/116) - Adds PSO, SNES, XNES, ADAM, and IPropMin optimisers to PintsOptimisers() class
+- [#38](https://github.com/pybop-team/PyBOP/issues/38) - Restructures the Problem classes ahead of adding a design optimisation example
 
 # [v23.11](https://github.com/pybop-team/PyBOP/releases/tag/v23.11)
 - Initial release
 - Adds Pints, NLOpt, and SciPy optimisers
 - Adds SumofSquareError and RootMeanSquareError cost functions
-- Adds Parameter and dataset classes
+- Adds Parameter and Dataset classes
--- tests/unit/test_problem.py
+++ tests/unit/test_problem.py
@@ -9,20 +9,25 @@ class TestProblem:
     A class to test the problem class.
     """
 
-    @pytest.mark.unit
-    def test_base_problem(self):
-        # Define model
+    @pytest.fixture
+    def x0(self):
+        return np.array([0.52, 0.63])
+
+    @pytest.fixture
+    def model(self, x0):
         model = pybop.lithium_ion.SPM()
         model.parameter_set = model.pybamm_model.default_parameter_values
-        x0 = np.array([0.52, 0.63])
         model.parameter_set.update(
             {
                 "Negative electrode active material volume fraction": x0[0],
                 "Positive electrode active material volume fraction": x0[1],
             }
         )
+        return model
 
-        parameters = [
+    @pytest.fixture
+    def parameters(self):
+        return [
             pybop.Parameter(
                 "Negative electrode active material volume fraction",
                 prior=pybop.Gaussian(0.5, 0.02),
@@ -35,6 +40,35 @@ class TestProblem:
             ),
         ]
 
+    @pytest.fixture
+    def experiment(self):
+        return pybamm.Experiment(
+            [
+                (
+                    "Discharge at 1C for 5 minutes (1 second period)",
+                    "Rest for 2 minutes (1 second period)",
+                    "Charge at 1C for 5 minutes (1 second period)",
+                    "Rest for 2 minutes (1 second period)",
+                ),
+            ]
+            * 2
+        )
+
+    @pytest.fixture
+    def dataset(self, model, experiment):
+        solution = model.predict(experiment=experiment)
+        return [
+            pybop.Dataset("Time [s]", solution["Time [s]"].data),
+            pybop.Dataset("Current function [A]", solution["Current [A]"].data),
+            pybop.Dataset("Voltage [V]", solution["Terminal voltage [V]"].data),
+        ]
+
+    @pytest.fixture
+    def signal(self):
+        return "Voltage [V]"
+
+    @pytest.mark.unit
+    def test_base_problem(self, parameters, model):
         # Test incorrect number of initial parameter values
         with pytest.raises(ValueError):
             pybop._problem.BaseProblem(parameters, model=model, x0=np.array([]))
@@ -50,41 +84,15 @@ class TestProblem:
             problem.evaluateS1([0.5, 0.5])
 
     @pytest.mark.unit
-    def test_fitting_problem(self):
-        # Define model
-        model = pybop.lithium_ion.SPM()
-        parameters = [
-            pybop.Parameter(
-                "Negative electrode active material volume fraction",
-                prior=pybop.Gaussian(0.5, 0.02),
-                bounds=[0.375, 0.625],
-            ),
-            pybop.Parameter(
-                "Positive electrode active material volume fraction",
-                prior=pybop.Gaussian(0.65, 0.02),
-                bounds=[0.525, 0.75],
-            ),
-        ]
-        signal = "Voltage [V]"
-
-        # Form dataset
-        x0 = np.array([0.52, 0.63])
-        solution = self.getdata(model, x0)
-
-        dataset = [
-            pybop.Dataset("Time [s]", solution["Time [s]"].data),
-            pybop.Dataset("Current function [A]", solution["Current [A]"].data),
-            pybop.Dataset("Voltage [V]", solution["Terminal voltage [V]"].data),
-        ]
-
+    def test_fitting_problem(self, parameters, dataset, model, signal):
         # Test incorrect number of initial parameter values
         with pytest.raises(ValueError):
             pybop.FittingProblem(
-                parameters, dataset, model=model, signal=signal, x0=np.array([])
+                parameters, dataset, model, signal=signal, x0=np.array([])
             )
 
         # Construct Problem
-        problem = pybop.FittingProblem(parameters, dataset, model=model, signal=signal)
+        problem = pybop.FittingProblem(parameters, dataset, model, signal=signal)
 
         assert problem._model == model
         assert problem._model._built_model is not None
@@ -92,76 +100,19 @@ class TestProblem:
         # Test model.simulate
         model.simulate(inputs=[0.5, 0.5], t_eval=np.linspace(0, 10, 100))
 
-    def getdata(self, model, x0):
-        model.parameter_set = model.pybamm_model.default_parameter_values
-
-        model.parameter_set.update(
-            {
-                "Negative electrode active material volume fraction": x0[0],
-                "Positive electrode active material volume fraction": x0[1],
-            }
-        )
-        experiment = pybamm.Experiment(
-            [
-                (
-                    "Discharge at 1C for 5 minutes (1 second period)",
-                    "Rest for 2 minutes (1 second period)",
-                    "Charge at 1C for 5 minutes (1 second period)",
-                    "Rest for 2 minutes (1 second period)",
-                ),
-            ]
-            * 2
-        )
-        sim = model.predict(experiment=experiment)
-        return sim
-
-    def test_design_problem(self):
-        # Define model
-        model = pybop.lithium_ion.SPM()
-        model.parameter_set = model.pybamm_model.default_parameter_values
-        x0 = np.array([0.52, 0.63])
-        model.parameter_set.update(
-            {
-                "Negative electrode active material volume fraction": x0[0],
-                "Positive electrode active material volume fraction": x0[1],
-            }
-        )
-
-        # Define target experiment
-        experiment = pybamm.Experiment(
-            [
-                (
-                    "Discharge at 1C for 5 minutes (1 second period)",
-                    "Rest for 2 minutes (1 second period)",
-                    "Charge at 1C for 5 minutes (1 second period)",
-                    "Rest for 2 minutes (1 second period)",
-                ),
-            ]
-            * 2
-        )
-
-        parameters = [
-            pybop.Parameter(
-                "Negative electrode active material volume fraction",
-                prior=pybop.Gaussian(0.5, 0.02),
-                bounds=[0.375, 0.625],
-            ),
-            pybop.Parameter(
-                "Positive electrode active material volume fraction",
-                prior=pybop.Gaussian(0.65, 0.02),
-                bounds=[0.525, 0.75],
-            ),
-        ]
-
+    @pytest.mark.unit
+    def test_design_problem(self, parameters, experiment, model):
         # Test incorrect number of initial parameter values
         with pytest.raises(ValueError):
-            pybop.DesignProblem(parameters, experiment, model=model, x0=np.array([]))
+            pybop.DesignProblem(parameters, experiment, model, x0=np.array([]))
 
         # Construct Problem
-        problem = pybop.DesignProblem(parameters, experiment, model=model)
+        problem = pybop.DesignProblem(parameters, experiment, model)
 
         assert problem._model == model
-        # problem._model._built_model is None (with an experiment)
+        assert (
+            problem._model._built_model is None
+        )  # building postponed with input experiment
 
         # Test model.predict
         model.predict(inputs=[0.5, 0.5], experiment=experiment)
