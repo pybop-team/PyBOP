1cdea9e18d41f3fc69531894ceb3736b461c6c68
NicolaCourtier <45851982+NicolaCourtier@users.noreply.github.com>
2024-02-08 16:43:58 +0000

    Update UKF example plotting

--- examples/scripts/exp_UKF.py
+++ examples/scripts/exp_UKF.py
@@ -101,7 +101,7 @@ x, final_cost = optim.run()
 print("Estimated parameters:", x)
 
 # Plot the timeseries output (requires model that returns Voltage)
-pybop.quick_plot(x, cost, title="Optimised Comparison")
+pybop.quick_plot(observer, parameter_values=x, title="Optimised Comparison")
 
 # Plot convergence
 pybop.plot_convergence(optim)
@@ -109,8 +109,5 @@ pybop.plot_convergence(optim)
 # Plot the parameter traces
 pybop.plot_parameters(optim)
 
-# Plot the cost landscape
-pybop.plot_cost2d(cost, steps=15)
-
 # Plot the cost landscape with optimisation path
-pybop.plot_cost2d(cost, optim=optim, steps=15)
+pybop.plot_optim2d(optim, steps=15)
