7e11e0b4e311a68bcd848d6e20d2b69e971b9aeb
Brady Planden <brady.planden@gmail.com>
2024-02-21 09:45:14 +0000

    Add tests for rebuild, and increase pybop.parameter tests

--- tests/unit/test_parameters.py
+++ tests/unit/test_parameters.py
@@ -7,15 +7,17 @@ class TestParameters:
     A class to test the parameter classes.
     """
 
-    @pytest.mark.unit
-    def test_parameter_construction(self):
-        parameter = pybop.Parameter(
+    @pytest.fixture
+    def parameter(self):
+        return pybop.Parameter(
             "Negative electrode active material volume fraction",
             prior=pybop.Gaussian(0.6, 0.02),
             bounds=[0.375, 0.7],
             initial_value=0.6,
         )
 
+    @pytest.mark.unit
+    def test_parameter_construction(self, parameter):
         assert parameter.name == "Negative electrode active material volume fraction"
         assert parameter.prior.mean == 0.6
         assert parameter.prior.sigma == 0.02
@@ -23,40 +25,19 @@ class TestParameters:
         assert parameter.initial_value == 0.6
 
     @pytest.mark.unit
-    def test_parameter_repr(self):
-        parameter = pybop.Parameter(
-            "Negative electrode active material volume fraction",
-            prior=pybop.Gaussian(0.6, 0.02),
-            bounds=[0.375, 0.7],
-            initial_value=0.6,
-        )
-
+    def test_parameter_repr(self, parameter):
         assert (
             repr(parameter)
             == "Parameter: Negative electrode active material volume fraction \n Prior: Gaussian, mean: 0.6, sigma: 0.02 \n Bounds: [0.375, 0.7] \n Value: 0.6"
         )
 
     @pytest.mark.unit
-    def test_parameter_rvs(self):
-        parameter = pybop.Parameter(
-            "Negative electrode active material volume fraction",
-            prior=pybop.Gaussian(0.6, 0.02),
-            bounds=[0.375, 0.7],
-            initial_value=0.6,
-        )
-
+    def test_parameter_rvs(self, parameter):
         samples = parameter.rvs(n_samples=500)
         assert (samples >= 0.375).all() and (samples <= 0.7).all()
 
     @pytest.mark.unit
-    def test_parameter_update(self):
-        parameter = pybop.Parameter(
-            "Negative electrode active material volume fraction",
-            prior=pybop.Gaussian(0.6, 0.02),
-            bounds=[0.375, 0.7],
-            initial_value=0.6,
-        )
-
+    def test_parameter_update(self, parameter):
         # Test value update
         parameter.update(value=0.534)
         assert parameter.value == 0.534
@@ -64,3 +45,9 @@ class TestParameters:
         # Test initial value update
         parameter.update(initial_value=0.654)
         assert parameter.value == 0.654
+
+    @pytest.mark.unit
+    def test_parameter_margin(self, parameter):
+        assert parameter.margin == 1e-4
+        parameter.set_margin(margin=1e-3)
+        assert parameter.margin == 1e-3
