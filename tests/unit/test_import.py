fed4a125e2d8a85c96be70050caa87ccd2529f97
Brady Planden <55357039+BradyPlanden@users.noreply.github.com>
2024-04-22 21:33:15 +0100

    Update tests/unit/test_import.py
    Co-authored-by: Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>

--- tests/unit/test_import.py
+++ tests/unit/test_import.py
@@ -28,7 +28,7 @@ class TestImport:
     def unload_pybop(self):
         """
         Unload pybop and its sub-modules. Credit PyBaMM team:
-        https://github.com/pybamm-team/PyBaMM/blob/develop/tests/unit/test_util.py
+        https://github.com/pybamm-team/PyBaMM/blob/90c1c357a97dfd5c8c6a9092a70dddf0dac978db/tests/unit/test_util.py
         """
         # Unload pybop and its sub-modules
         for module_name in list(sys.modules.keys()):
