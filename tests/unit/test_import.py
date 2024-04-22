9504925079393ad3d1aff553f5c30264db5c5cd9
Brady Planden <brady.planden@gmail.com>
2024-04-22 18:23:54 +0100

    Updt test_import with module delete and import

--- tests/unit/test_import.py
+++ tests/unit/test_import.py
@@ -4,16 +4,14 @@ from unittest.mock import patch
 
 import pytest
 
-import pybop
-
 
 @pytest.mark.unit
 def test_multiprocessing_init_non_win32(monkeypatch):
     """Test multiprocessing init on non-Windows platforms"""
     monkeypatch.setattr(sys, "platform", "linux")
-    print(sys.platform)
     with patch("multiprocessing.set_start_method") as mock_set_start_method:
-        importlib.reload(pybop)
+        del sys.modules["pybop"]
+        importlib.import_module("pybop")
         mock_set_start_method.assert_called_once_with("fork")
 
 
@@ -21,6 +19,7 @@ def test_multiprocessing_init_non_win32(monkeypatch):
 def test_multiprocessing_init_win32(monkeypatch):
     """Test multiprocessing init on Windows"""
     monkeypatch.setattr(sys, "platform", "win32")
+    del sys.modules["pybop"]
     with patch("multiprocessing.set_start_method") as mock_set_start_method:
-        importlib.reload(pybop)
+        importlib.import_module("pybop")
         mock_set_start_method.assert_called_once_with("spawn")
