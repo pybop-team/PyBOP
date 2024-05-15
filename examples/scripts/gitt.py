d38b38f8f621c945cb4ad96595b950b8afd999bc
Ferran Brosa Planella <Ferran.Brosa-Planella@warwick.ac.uk>
2024-05-15 14:10:55 +0100

    #223 remove GITT class and move to example

--- examples/scripts/gitt.py
+++ examples/scripts/gitt.py
@@ -33,9 +33,24 @@ parameter_set.update(
 )
 
 # Define the cost to optimise
-problem = pybop.GITT(
-    model="Weppner & Huggins", parameter_set=parameter_set, dataset=dataset
+model = pybop.lithium_ion.WeppnerHuggins(parameter_set=parameter_set)
+
+parameters = [
+    pybop.Parameter(
+        "Positive electrode diffusivity [m2.s-1]",
+        prior=pybop.Gaussian(5e-14, 1e-13),
+        bounds=[1e-16, 1e-11],
+        true_value=parameter_set["Positive electrode diffusivity [m2.s-1]"],
+    ),
+]
+
+problem = pybop.FittingProblem(
+    model,
+    parameters,
+    dataset,
+    signal=["Voltage [V]"],
 )
+
 cost = pybop.RootMeanSquaredError(problem)
 
 # Build the optimisation problem
