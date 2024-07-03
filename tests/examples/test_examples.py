aadef82ee035b66b5a18ab9e266b11d3c4c53d24
Brady Planden <brady.planden@gmail.com>
2024-07-03 13:05:28 +0100

    fixes: stricter ruff settings - 7, A001

--- tests/examples/test_examples.py
+++ tests/examples/test_examples.py
@@ -12,14 +12,14 @@ class TestExamples:
     """
 
     def list_of_examples():
-        list = []
+        examples_list = []
         path_to_example_scripts = os.path.join(
             pybop.script_path, "..", "examples", "scripts"
         )
         for example in os.listdir(path_to_example_scripts):
             if example.endswith(".py"):
-                list.append(os.path.join(path_to_example_scripts, example))
-        return list
+                examples_list.append(os.path.join(path_to_example_scripts, example))
+        return examples_list
 
     @pytest.mark.parametrize("example", list_of_examples())
     @pytest.mark.examples
