fix: Incorrect likelihood calculation, add dynamic bounds to sigma, remove redundant return calcs

pybop/costs/_likelihoods.py
@@ -65,7 +65,7 @@ class GaussianLogLikelihoodKnownSigma(BaseLikelihood):
             dl = np.sum((np.sum((r * self.dy.T), axis=2) / self.sigma2), axis=1)
             return e, dl
 
-        return e.item() if self.n_outputs == 1 else np.sum(e)
+        return e
 
     def check_sigma0(self, sigma0: Union[np.ndarray, float]):
         """
@@ -139,7 +139,7 @@ class GaussianLogLikelihood(BaseLikelihood):
                     f"Sigma for output {index+1}",
                     initial_value=value,
                     prior=Uniform(0.5 * value, 1.5 * value),
-                    # bounds = [1e-6, 1.0], # TODO: This should have a lower bound of zero and no higher bound
+                    bounds=[1e-8, 3 * value],
                 )
             )
         else:
@@ -208,7 +208,7 @@ class GaussianLogLikelihood(BaseLikelihood):
         e = np.sum(
             self._logpi
             - self.n_time_data * np.log(sigma)
-            - np.sum((r) ** 2.0) / (2.0 * sigma**2.0)
+            - np.sum(r**2.0, axis=1) / (2.0 * sigma**2.0)
         )
 
         if calculate_grad:
@@ -217,9 +217,9 @@ class GaussianLogLikelihood(BaseLikelihood):
                 -self.n_time_data / sigma + np.sum(r**2.0, axis=1) / (sigma**3.0)
             ) / self._dsigma_scale
             dl = np.concatenate((dl.flatten(), dsigma))
-            return (e.item(), dl) if self.n_outputs == 1 else (np.sum(e), dl)
+            return e, dl
 
-        return e.item() if self.n_outputs == 1 else np.sum(e)
+        return e
 
 
 class MAP(BaseLikelihood):

pybop/costs/fitting_costs.py
@@ -63,7 +63,7 @@ class RootMeanSquaredError(BaseCost):
             return (
                 (e.item(), de.flatten())
                 if self.n_outputs == 1
-                else (np.sum(e), np.sum(de, axis=1))
+                else (e.sum(), de.sum(1))
             )
 
         return e.item() if self.n_outputs == 1 else np.sum(e)
@@ -125,9 +125,8 @@ class SumSquaredError(BaseCost):
         if calculate_grad is True:
             de = 2 * np.sum(np.sum((r * self.dy.T), axis=2), axis=1)
             return e, de
-            # return e.item(), de.flatten() if self.n_outputs == 1 else np.sum(e), np.sum(de, axis=1) #TODO: double check the output size on this
 
-        return e.item() if self.n_outputs == 1 else np.sum(e)
+        return e
 
 
 class Minkowski(BaseCost):
@@ -201,13 +200,7 @@ class Minkowski(BaseCost):
         r = np.asarray(
             [self.y[signal] - self._target[signal] for signal in self.signal]
         )
-        e = np.asarray(
-            [
-                np.sum(np.abs(self.y[signal] - self._target[signal]) ** self.p)
-                ** (1 / self.p)
-                for signal in self.signal
-            ]
-        )
+        e = np.sum(np.abs(r) ** self.p) ** (1 / self.p)
 
         if calculate_grad is True:
             de = np.sum(
@@ -215,9 +208,9 @@ class Minkowski(BaseCost):
                 / (e ** (self.p - 1) + np.finfo(float).eps),
                 axis=1,
             )
-            return np.sum(e), de
+            return e, de
 
-        return e.item() if self.n_outputs == 1 else np.sum(e)
+        return e
 
 
 class SumofPower(BaseCost):