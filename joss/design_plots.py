4372e72c11dcac4aa014181418d2d0529bb8910f
NicolaCourtier <45851982+NicolaCourtier@users.noreply.github.com>
2024-08-25 04:41:20 +0100

    Add example design plots

--- joss/design_plots.py
+++ joss/design_plots.py
@@ -1,7 +1,18 @@
-
 # A script to generate design optimisation plots for the JOSS paper.
 
+import numpy as np
+
 import pybop
+from pybop import PlotlyManager
+
+go = PlotlyManager().go
+np.random.seed(8)
+
+# Choose which plots to show and save
+create_plot = {}
+create_plot["gravimetric"] = True  # takes longer
+create_plot["prediction"] = True
+
 
 # Define parameter set and model
 parameter_set = pybop.ParameterSet.pybamm("Chen2020", formation_concentrations=True)
@@ -13,18 +24,18 @@ parameters = pybop.Parameters(
         "Positive electrode thickness [m]",
         initial_value=7.56e-05,
         prior=pybop.Gaussian(7.56e-05, 3e-05),
-        bounds=[7e-05, 14e-05],
+        bounds=[65e-06, 130e-06],
     ),
     pybop.Parameter(
         "Positive electrode active material volume fraction",
         initial_value=0.58,
-        prior=pybop.Gaussian(0.58, 0.05),
+        prior=pybop.Gaussian(0.58, 0.1),
         bounds=[0.3, 0.9],
     ),
 )
 
 # Define test protocol
-experiment = pybop.Experiment(["Discharge at 1C until 2.5 V (5 seconds period)"])
+experiment = pybop.Experiment(["Discharge at 1C until 2.5 V (10 second period)"])
 
 # Generate problem, cost and optimiser classes
 problem = pybop.DesignProblem(
@@ -33,10 +44,10 @@ problem = pybop.DesignProblem(
     experiment,
     signal=["Voltage [V]", "Current [A]"],
     initial_state={"Initial SoC": 1.0},
-    # update_capacity=True,
+    update_capacity=True,
 )
 cost = pybop.GravimetricEnergyDensity(problem)
-optim = pybop.XNES(
+optim = pybop.PSO(
     cost,
     verbose=True,
     allow_infeasible_solutions=False,
@@ -50,8 +61,24 @@ print("Estimated parameters:", x)
 print(f"Initial gravimetric energy density: {cost(optim.x0):.2f} Wh.kg-1")
 print(f"Optimised gravimetric energy density: {cost(x):.2f} Wh.kg-1")
 
-# Plot the timeseries output
-pybop.quick_plot(problem, problem_inputs=x, title="Optimised Comparison")
+if create_plot["gravimetric"]:
+    # Plot the cost landscape with optimisation path
+    gravimetric_fig = pybop.plot2d(optim, steps=25, title=None)
+    gravimetric_fig.write_image("joss/figures/design_gravimetric.png")
 
-# Plot the cost landscape with optimisation path
-pybop.plot2d(optim, steps=15)
+if create_plot["prediction"]:
+    # Plot the timeseries output
+    figs = pybop.quick_plot(
+        problem,
+        problem_inputs=x,
+        title=None,
+        width=576,
+        height=576,
+        legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1),
+        show=False,
+    )
+    prediction_fig = figs[0]
+    prediction_fig.data[0].name = f"Initial result: {cost(optim.x0):.2f} Wh.kg-1"
+    prediction_fig.data[1].name = f"Optimised result: {cost(x):.2f} Wh.kg-1"
+    prediction_fig.show()
+    prediction_fig.write_image("joss/figures/design_prediction.png")
