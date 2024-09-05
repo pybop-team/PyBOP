03e096ca60d6f5508930281f6ecad64492678c74
Brady Planden <brady.planden@gmail.com>
2024-09-05 09:22:20 +0100

    fix: updates for pybamm v24.9, adds IDAKLU t_interp functionality

--- examples/scripts/spm_IRPropMin.py
+++ examples/scripts/spm_IRPropMin.py
@@ -1,10 +1,12 @@
 import numpy as np
+import pybamm
 
 import pybop
 
-# Define model
+# Define model and use high-performant solver for sensitivities
+solver = pybamm.IDAKLUSolver()
 parameter_set = pybop.ParameterSet.pybamm("Chen2020")
-model = pybop.lithium_ion.SPM(parameter_set=parameter_set)
+model = pybop.lithium_ion.SPM(parameter_set=parameter_set, solver=solver)
 
 # Fitting parameters
 parameters = pybop.Parameters(
@@ -36,7 +38,7 @@ dataset = pybop.Dataset(
 # Generate problem, cost function, and optimisation class
 problem = pybop.FittingProblem(model, parameters, dataset)
 cost = pybop.Minkowski(problem, p=2)
-optim = pybop.IRPropMin(cost, max_iterations=100)
+optim = pybop.AdamW(cost, min_iterations=100, max_iterations=100)
 
 x, final_cost = optim.run()
 print("Estimated parameters:", x)
