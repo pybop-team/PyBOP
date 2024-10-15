c04ff9dc1c7514acacf87404e14a05679f485ae5
Brady Planden <brady.planden@gmail.com>
2024-10-15 18:57:53 +0100

    tests: coverage for PosteriorSummary()

--- tests/unit/test_plots.py
+++ tests/unit/test_plots.py
@@ -141,6 +141,29 @@ class TestPlots:
         # Plot gradient cost landscape
         pybop.plot2d(optim, gradient=True, steps=5)
 
+    @pytest.fixture
+    def posterior_summary(self, fitting_problem):
+        posterior = pybop.LogPosterior(
+            pybop.GaussianLogLikelihoodKnownSigma(fitting_problem, sigma0=2e-3)
+        )
+        sampler = pybop.SliceStepoutMCMC(posterior, chains=1, iterations=1)
+        results = sampler.run()
+        return pybop.PosteriorSummary(results)
+
+    @pytest.mark.unit
+    def test_posterior_plots(self, posterior_summary):
+        # Plot trace
+        posterior_summary.plot_trace()
+
+        # Plot posterior
+        posterior_summary.plot_posterior()
+
+        # Plot chains
+        posterior_summary.plot_chains()
+
+        # Plot summary table
+        posterior_summary.summary_table()
+
     @pytest.mark.unit
     def test_with_ipykernel(self, dataset, cost, optim):
         import ipykernel
