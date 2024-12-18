896139f10eadf7f1b4a7ba9cd34bd448c9a2489f
Dibyendu-IITKGP <dibyendukgec@gmail.com>
2024-12-18 20:13:10 +0000

    unit tests modified

--- tests/unit/test_optimisation.py
+++ tests/unit/test_optimisation.py
@@ -340,33 +340,18 @@ class TestOptimisation:
 
     @pytest.mark.unit
     def test_randomsearch_bounds(self, two_param_cost):
-        # Initialize RandomSearch with boundaries
+        # Test clip_candidates with bound
         bounds = {"upper": [0.62,0.57], "lower": [0.58,0.53]}
-
         optimiser = pybop.RandomSearch(cost=two_param_cost, bounds=bounds, max_iterations=1)
-
-        # Define candidates outside boundaries
         candidates = np.array([[0.57, 0.52], [0.63, 0.58]])
-
-        # Use clip_candidates to clip to boundaries
         clipped_candidates = optimiser.optimiser.clip_candidates(candidates)
-
-        # Expected clipped candidates
         expected_clipped = np.array([[0.58, 0.53], [0.62, 0.57]])
-
-        # Assert that the clipped candidates match the expected values
         assert np.allclose(clipped_candidates, expected_clipped)
 
-        # Initialize optimiser without boundaries
+        # Test clip_candidates without bound
         optimiser = pybop.RandomSearch(cost=two_param_cost, bounds=None, max_iterations=1)
-
-        # Define candidates outside typical boundaries
         candidates = np.array([[0.57, 0.52], [0.63, 0.58]])
-
-        # Use clip_candidates, which should return the input unmodified
         clipped_candidates = optimiser.optimiser.clip_candidates(candidates)
-
-        # Assert that the clipped candidates are unchanged
         assert np.allclose(clipped_candidates, candidates)
 
     @pytest.mark.unit
