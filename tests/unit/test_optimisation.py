03a7a05a38438c8e3cdf74262925d9f67f15de79
Dibyendu-IITKGP <dibyendukgec@gmail.com>
2024-12-18 12:46:26 +0000

    unit test modified

--- tests/unit/test_optimisation.py
+++ tests/unit/test_optimisation.py
@@ -324,11 +324,6 @@ class TestOptimisation:
                 assert optim.optimiser.n_hyper_parameters() == 5
                 assert optim.optimiser.x_guessed() == optim.optimiser._x0
 
-            # if optimiser is pybop.RandomSearch:
-            #     assert optimiser._dim == len(optim.optimiser._x0)
-            #     assert optimiser.x_best().shape == optim.optimiser._x0.shape
-            #     assert optimiser.f_best() == np.inf
-            #     assert optimiser._iterations == 0
         else:
             x0 = cost.parameters.initial_value()
             assert optim.x0 == x0
