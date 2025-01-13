295247419456137387763e8a0abd434829110bb4
Brady Planden <brady.planden@gmail.com>
2025-01-13 17:11:52 +0000

    tests: adds MAE, MSE costs to eis integration.

--- pybop/optimisers/_cuckoo.py
+++ pybop/optimisers/_cuckoo.py
@@ -204,6 +204,7 @@ class CuckooSearchImpl(PopulationBasedOptimiser):
 
     @pa.setter
     def pa(self, pa):
+        """Setter for abandonment rate"""
         if not isinstance(pa, (int, float)) or not 0 < pa <= 1:
             raise ValueError("pa must be a numeric value between 0 and 1.")
         self._pa = float(pa)
