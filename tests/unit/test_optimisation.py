be2f192a2366123dd6d0bae8db630d0e3a1ba4d8
Brady Planden <brady.planden@gmail.com>
2025-01-28 13:27:13 +0000

    fix: add missed test

--- tests/unit/test_optimisation.py
+++ tests/unit/test_optimisation.py
@@ -663,7 +663,7 @@ class TestOptimisation:
 
         assert (
             "Positive electrode active material volume fraction"
-            in results.pybamm_solution[0].all_inputs[0]
+            in results.pybamm_solution.all_inputs[0]
         )
 
         optim.set_max_unchanged_iterations()
