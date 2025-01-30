fd2b3f3d2a8b559ba625a76c501e92fe954d5913
NicolaCourtier <45851982+NicolaCourtier@users.noreply.github.com>
2025-01-30 23:11:41 +0000

    Create GITTFit with example

---
+++ examples/scripts/battery_parameterisation/gitt_fitting.py
@@ -0,0 +1,82 @@
+import numpy as np
+
+import pybop
+from pybop.models.lithium_ion.basic_SP_diffusion import (
+    convert_physical_to_electrode_parameters,
+)
+from pybamm import Interpolant
+
+# Define model
+parameter_set = pybop.ParameterSet("Xu2019")
+model = pybop.lithium_ion.SPMe(
+    parameter_set=parameter_set, options={"working electrode": "positive"}
+)
+
+# Generate data
+sigma = 1e-3
+initial_state = {"Initial SoC": 0.9}
+experiment = pybop.Experiment(
+    [
+        "Rest for 1 second",
+        ("Discharge at 1C for 10 minutes (10 second period)", "Rest for 20 minutes") * 8,
+    ]
+)
+values = model.predict(initial_state=initial_state, experiment=experiment)
+corrupt_values = values["Voltage [V]"].data + np.random.normal(
+    0, sigma, len(values["Voltage [V]"].data)
+)
+
+# Form dataset and locate the pulse
+dataset = pybop.Dataset(
+    {
+        "Time [s]": values["Time [s]"].data,
+        "Current function [A]": values["Current [A]"].data,
+        "Discharge capacity [A.h]": values["Discharge capacity [A.h]"].data,
+        "Voltage [V]": corrupt_values,
+    }
+)
+
+# Determine the indices corresponding to each pulse in the dataset
+nonzero_index = np.concatenate(
+    ([-1], np.flatnonzero(dataset["Current function [A]"]), [len(dataset["Current function [A]"])+1])
+)
+nonzero_gaps = np.extract(
+    nonzero_index[1:] - nonzero_index[:-1] != 1,  # check if there is a gap
+    nonzero_index[1:],  # return the index at the start of the pulse
+)
+pulse_index = []
+for start, finish in zip(nonzero_gaps[:-1], nonzero_gaps[1:]):
+    pulse_index.append([i for i in nonzero_index if i >= start and i<finish])
+
+# Define parameter set
+parameter_set = convert_physical_to_electrode_parameters(
+    model.parameter_set, "positive"
+)
+
+# Fit the whole GITT measurement
+gitt_fit = pybop.GITTFit(dataset, pulse_index, parameter_set)
+
+# Plot the parameters
+pybop.plot.dataset(gitt_fit.parameter_data, signal=["Particle diffusion time scale [s]"])
+pybop.plot.dataset(gitt_fit.parameter_data, signal=["Series resistance [Ohm]"])
+
+# Replace the diffusivity in the original model
+parameter_set = pybop.ParameterSet("Xu2019")
+
+def diffusivity(sto, T):
+    return parameter_set["Positive particle radius [m]"]**2 / Interpolant(
+        gitt_fit.parameter_data["Stoichiometry"],
+        gitt_fit.parameter_data["Particle diffusion time scale [s]"],
+        sto,
+        name="Diffusivity",
+    )
+
+
+# Compare the original and identified model predictions
+parameter_set.update({"Positive particle diffusivity [m2.s-1]": diffusivity})
+values = model.predict(initial_state=initial_state, experiment=experiment, parameter_set=parameter_set)
+pybop.plot.trajectories(
+    values["Time [s]"].data,
+    [dataset["Voltage [V]"], values["Voltage [V]"].data],
+    trace_names=["Ground truth", "Identified"],
+)
--- pybop/applications/gitt_methods.py
+++ pybop/applications/gitt_methods.py
@@ -1,5 +1,7 @@
 from typing import Optional
 
+import numpy as np
+
 import pybop
 from pybop import BaseApplication
 
@@ -46,7 +48,9 @@ class GITTPulseFit(BaseApplication):
 
         # Define the cost to optimise
         self.parameter_set = parameter_set
-        self.model = pybop.lithium_ion.SPDiffusion(parameter_set=self.parameter_set)
+        self.model = pybop.lithium_ion.SPDiffusion(
+            parameter_set=self.parameter_set, build=True
+        )
         self.problem = pybop.FittingProblem(self.model, self.parameters, gitt_pulse)
         self.cost = cost(self.problem)
 
@@ -54,3 +58,83 @@ class GITTPulseFit(BaseApplication):
         self.optim = optimiser(cost=self.cost, verbose=verbose)
         self.results = self.optim.run()
         self.parameter_set.update(self.parameters.as_dict(self.results.x))
+
+
+class GITTFit(BaseApplication):
+    """
+    Fit the diffusion timescale of each pulse from a galvanostatic intermittent
+    titration technique (GITT) measurement.
+
+    Parameters
+    ----------
+    gitt_dataset : pybop.Dataset
+        A dataset containing the "Time [s]", "Current [A]" and "Voltage [V]" for one
+        pulse obtained from a GITT measurement.
+    gitt_pulse_index : list[np.ndarray]
+        A nested list of integers representing the indices of each pulse in the dataset.
+    parameter_set : pybop.ParameterSet
+        A parameter set containing values for the parameters of the SPDiffusion model.
+    cost : pybop.BaseCost, optional
+        The cost function to quantify the error (default: pybop.RootMeanSquaredError).
+    optimiser : pybop.BaseOptimiser, optional
+        The optimisation algorithm to use (default: pybop.SciPyMinimize).
+    verbose : bool, optional
+        If True, progress messages are printed (default: False).
+    """
+
+    def __init__(
+        self,
+        gitt_dataset: pybop.Dataset,
+        gitt_pulse_index: list[np.ndarray],
+        parameter_set: pybop.ParameterSet,
+        cost: Optional[pybop.BaseCost] = pybop.RootMeanSquaredError,
+        optimiser: Optional[pybop.BaseOptimiser] = pybop.SciPyMinimize,
+        verbose: bool = False,
+    ):
+        # Preallocate outputs
+        self.pulses = []
+        stoichiometry = []
+        diffusion_time = []
+        series_resistance = []
+
+        for pulse_index in gitt_pulse_index:
+            # Estimate the parameters for this pulse
+            self.pulses.append(
+                pybop.GITTPulseFit(
+                    gitt_pulse=gitt_dataset.get_subset(pulse_index),
+                    parameter_set=parameter_set,
+                    cost=cost,
+                    optimiser=optimiser,
+                    verbose=verbose,
+                )
+            )
+
+            # Log and update the parameter estimates for the next iteration
+            parameter_set.update(
+                {
+                    "Particle diffusion time scale [s]": self.pulses[-1].results.x[0],
+                    "Series resistance [Ohm]": self.pulses[-1].results.x[1],
+                }
+            )
+            diffusion_time.append(parameter_set["Particle diffusion time scale [s]"])
+            series_resistance.append(parameter_set["Series resistance [Ohm]"])
+            stoichiometry.append(parameter_set["Initial stoichiometry"])
+            parameter_set.update(
+                {
+                    "Initial stoichiometry": stoichiometry[-1]
+                    + (
+                        gitt_dataset["Discharge capacity [A.h]"][pulse_index[-1] + 1]
+                        - gitt_dataset["Discharge capacity [A.h]"][pulse_index[0] - 1]
+                    )
+                    / (parameter_set["Theoretical electrode capacity [A.s]"] / 3600)
+                }
+            )
+
+        self.parameter_data = pybop.Dataset(
+            {
+                "Stoichiometry": np.asarray(stoichiometry),
+                "Particle diffusion time scale [s]": np.asarray(diffusion_time),
+                "Series resistance [Ohm]": np.asarray(series_resistance),
+            },
+            domain="Stoichiometry",
+        )
