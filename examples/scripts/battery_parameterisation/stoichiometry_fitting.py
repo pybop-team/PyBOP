caf028bcd8f0332c2dd390a9243785d74eadfdf6
NicolaCourtier <45851982+NicolaCourtier@users.noreply.github.com>
2025-01-30 23:18:41 +0000

    Update stoichiometry_fitting.py

--- examples/scripts/battery_parameterisation/stoichiometry_fitting.py
+++ examples/scripts/battery_parameterisation/stoichiometry_fitting.py
@@ -21,7 +21,7 @@ ocv_dataset = pybop.Dataset(
 )
 
 # Estimate the stoichiometry corresponding to the GITT-OCV
-ocv_fit = pybop.stoichiometric_fit(
+ocv_fit = pybop.StoichiometricFit(
     ocv_dataset,
     ocv_function,
 )
