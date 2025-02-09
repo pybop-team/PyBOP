Fix Gaussian logpdf gradient

pybop/costs/_likelihoods.py
@@ -342,6 +342,7 @@ class LogPosterior(BaseMetaLikelihood):
         Union[float, Tuple[float, np.ndarray]]
             The posterior cost, and optionally the gradient.
         """
+        # Compute log prior (and gradient)
         if dy is not None:
             if isinstance(self._prior, BasePrior):
                 log_prior, dp = self._prior.logpdfS1(self.parameters.current_value())
@@ -350,19 +351,19 @@ class LogPosterior(BaseMetaLikelihood):
                 log_prior = self._prior.logpdf(self.parameters.current_value())
 
                 # Compute a finite difference approximation of the gradient of the log prior
-                delta = self.parameters.initial_value() * self.gradient_step
+                delta = self.parameters.current_value() * self.gradient_step
                 dp = []
 
                 for parameter, step_size in zip(self.parameters, delta):
                     param_value = parameter.value
-                    upper_value = param_value * (1 + step_size)
-                    lower_value = param_value * (1 - step_size)
+                    upper_value = param_value + step_size
+                    lower_value = param_value - step_size
 
-                    log_prior_upper = parameter.prior.logpdf(upper_value)
-                    log_prior_lower = parameter.prior.logpdf(lower_value)
+                    log_prior_upper = self._prior.logpdf(upper_value)
+                    log_prior_lower = self._prior.logpdf(lower_value)
 
                     gradient = (log_prior_upper - log_prior_lower) / (
-                        2 * step_size * param_value + np.finfo(float).eps
+                        2 * step_size + np.finfo(float).eps
                     )
                     dp.append(gradient)
         else:
@@ -371,6 +372,7 @@ class LogPosterior(BaseMetaLikelihood):
         if not np.isfinite(log_prior).any():
             return (-np.inf, -self.grad_fail) if dy is not None else -np.inf
 
+        # Compute log likelihood and add log prior (and gradients)
         if dy is not None:
             log_likelihood, dl = self._log_likelihood.compute(y, dy)
 

tests/unit/test_cost_interface.py
@@ -13,8 +13,8 @@ class TestCostInterface:
 
     @pytest.fixture(autouse=True)
     def setup(self):
-        self.x_model = [0.5, 1.5]
-        self.x_search = [-2.5 * (self.x_model[0] + 1), np.log(self.x_model[1])]
+        self.x_model = [0.525, 1.5]
+        self.x_search = [1 / 0.25 * (self.x_model[0] - 0.375), np.log(self.x_model[1])]
 
     @pytest.fixture
     def model(self):
@@ -28,7 +28,7 @@ class TestCostInterface:
                 prior=pybop.Gaussian(0.5, 0.01),
                 bounds=[0.375, 0.625],
                 transformation=pybop.ScaledTransformation(
-                    coefficient=-2.5, intercept=1
+                    coefficient=1 / 0.25, intercept=-0.375
                 ),
             ),
             pybop.Parameter(
@@ -147,9 +147,9 @@ class TestCostInterface:
 
         np.testing.assert_allclose(true_cost, cost_with_transformation)
 
-        delta = 1e-6
         numerical_grad = []
         for i in range(len(self.x_model)):
+            delta = 1e-6 * self.x_model[i]
             self.x_model[i] += delta / 2
             cost_right = cost(self.x_model)
             self.x_model[i] -= delta
@@ -157,7 +157,7 @@ class TestCostInterface:
             self.x_model[i] += delta / 2
             numerical_grad.append((cost_right - cost_left) / delta)
 
-        np.testing.assert_allclose(grad_wrt_model_parameters, numerical_grad, rtol=1e-4)
+        np.testing.assert_allclose(grad_wrt_model_parameters, numerical_grad, rtol=6e-4)
 
         jac = optim.transformation.jacobian(self.x_search)
         gradient_wrt_search_parameters = np.matmul(grad_wrt_model_parameters, jac)

tests/unit/test_posterior.py
@@ -118,10 +118,12 @@ class TestLogPosterior:
 
     def test_non_logpdfS1_prior(self, likelihood):
         # Scipy distribution
-        prior = st.norm(0.8, 0.01)
+        prior = st.norm(loc=0.8, scale=0.01)
         posterior = pybop.LogPosterior(likelihood, log_prior=prior)
+        l, dl = likelihood([0.6], calculate_grad=True)
         p, dp = posterior([0.6], calculate_grad=True)
 
-        # Assert to PyBOP.Gaussian
-        p2, dp2 = pybop.Gaussian(0.8, 0.01).logpdfS1(0.6)
-        np.testing.assert_allclose(dp, dp2, atol=2e-3)
+        # Assert to pybop.Gaussian
+        p2, dp2 = pybop.Gaussian(mean=0.8, sigma=0.01).logpdfS1(0.6)
+        np.testing.assert_allclose(p - l, p2, atol=2e-3)
+        np.testing.assert_allclose(dp - dl, dp2, atol=2e-3)