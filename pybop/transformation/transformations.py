Align samplers and fix cost gradient (#661)
* Remove x0 from optim.log

* Clarify x0 and bounds passing

* Update bounds to dict

* Sampler x0 corresponds to model parameters

* Separate out optimisation_result

* Update integration tests

* Return to combined single_call

* Align sampler with cost_call

* Fix test_spm_parameterisations

* Update parameters.get_bounds

* Move transformation to cost interface

* Rename test_cost_transformations

* Move minimising to cost interface

* Split apply_transformation

* Update transformation example and plotting

* Update CHANGELOG.md

* Improve transformation tests

* Align contour with current functionality

* Add contour test with transform

* Fix Gaussian logpdf gradient

* Increase coverage

* Apply suggestions from code review

Co-authored-by: Brady Planden <55357039+BradyPlanden@users.noreply.github.com>

* Add optim type

* Pass transformations and invert_cost to CostInterface

* Transform failed gradients to preserve direction

* Update missed test

* Pass x0 and bounds in model space

---------

Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
Co-authored-by: Brady Planden <55357039+BradyPlanden@users.noreply.github.com>

pybop/transformation/transformations.py
@@ -109,7 +109,7 @@ class ScaledTransformation(Transformation):
 
     def log_jacobian_det(self, q: np.ndarray) -> float:
         """See :meth:`Transformation.log_jacobian_det()`."""
-        return np.log(np.abs(self.coefficient)).sum()
+        return np.log(np.abs(self.inverse_coeff)).sum()
 
     def log_jacobian_det_S1(self, q: np.ndarray) -> tuple[float, np.ndarray]:
         """See :meth:`Transformation.log_jacobian_det_S1()`."""

tests/integration/test_spm_parameterisations.py
@@ -143,9 +143,10 @@ class Test_SPM_Parameterisation:
             pybop.SciPyDifferentialEvolution,
             pybop.CuckooSearch,
         ]:
-            common_args["bounds"] = [[0.375, 0.775], [0.375, 0.775]]
+            common_args["bounds"] = {"lower": [0.375, 0.375], "upper": [0.775, 0.775]}
             if isinstance(cost, pybop.GaussianLogLikelihood):
-                common_args["bounds"].extend([[0.0, 0.05]])
+                common_args["bounds"]["lower"].append(0.0)
+                common_args["bounds"]["upper"].append(0.05)
 
         # Set sigma0 and create optimiser
         optim = optimiser(**common_args)
@@ -176,16 +177,14 @@ class Test_SPM_Parameterisation:
         if np.allclose(x0, self.ground_truth, atol=1e-5):
             raise AssertionError("Initial guess is too close to ground truth")
 
-        if optim.minimising:
+        if results.minimising:
             assert initial_cost > results.final_cost
         else:
             assert initial_cost < results.final_cost
 
+        np.testing.assert_allclose(results.x, self.ground_truth, atol=1.5e-2)
         if isinstance(optim.cost, pybop.GaussianLogLikelihood):
-            np.testing.assert_allclose(results.x, self.ground_truth, atol=1.5e-2)
             np.testing.assert_allclose(results.x[-1], self.sigma0, atol=5e-4)
-        else:
-            np.testing.assert_allclose(results.x, self.ground_truth, atol=1.5e-2)
 
     @pytest.fixture
     def two_signal_cost(self, parameters, model, cost_cls):
@@ -241,9 +240,10 @@ class Test_SPM_Parameterisation:
         }
 
         if multi_optimiser is pybop.SciPyDifferentialEvolution:
-            common_args["bounds"] = [[0.375, 0.775], [0.375, 0.775]]
+            common_args["bounds"] = {"lower": [0.375, 0.375], "upper": [0.775, 0.775]}
             if isinstance(two_signal_cost, pybop.GaussianLogLikelihood):
-                common_args["bounds"].extend([[0.0, 0.05], [0.0, 0.05]])
+                common_args["bounds"]["lower"].extend([0.0, 0.0])
+                common_args["bounds"]["upper"].extend([0.05, 0.05])
 
         # Test each optimiser
         optim = multi_optimiser(**common_args)
@@ -259,16 +259,14 @@ class Test_SPM_Parameterisation:
         if np.allclose(x0, self.ground_truth, atol=1e-5):
             raise AssertionError("Initial guess is too close to ground truth")
 
-        if optim.minimising:
+        if results.minimising:
             assert initial_cost > results.final_cost
         else:
             assert initial_cost < results.final_cost
 
+        np.testing.assert_allclose(results.x, self.ground_truth, atol=1.5e-2)
         if isinstance(two_signal_cost, pybop.GaussianLogLikelihood):
-            np.testing.assert_allclose(results.x, self.ground_truth, atol=1.5e-2)
             np.testing.assert_allclose(results.x[-2:], combined_sigma0, atol=5e-4)
-        else:
-            np.testing.assert_allclose(results.x, self.ground_truth, atol=1.5e-2)
 
     @pytest.mark.parametrize("init_soc", [0.4, 0.6])
     def test_model_misparameterisation(self, parameters, model, init_soc):
@@ -312,11 +310,8 @@ class Test_SPM_Parameterisation:
         initial_state = {"Initial SoC": init_soc}
         experiment = pybop.Experiment(
             [
-                (
-                    "Discharge at 0.5C for 8 minutes (8 second period)",
-                    "Charge at 0.5C for 8 minutes (8 second period)",
-                )
+                "Discharge at 0.5C for 8 minutes (8 second period)",
+                "Charge at 0.5C for 8 minutes (8 second period)",
             ]
         )
-        sim = model.predict(initial_state=initial_state, experiment=experiment)
-        return sim
+        return model.predict(initial_state=initial_state, experiment=experiment)

tests/integration/test_thevenin_parameterisation.py
@@ -40,13 +40,13 @@ class TestTheveninParameterisation:
             pybop.Parameter(
                 "R0 [Ohm]",
                 prior=pybop.Gaussian(0.05, 0.01),
-                bounds=[0, 0.1],
+                bounds=[1e-6, 0.1],
                 transformation=pybop.LogTransformation(),
             ),
             pybop.Parameter(
                 "R1 [Ohm]",
                 prior=pybop.Gaussian(0.05, 0.01),
-                bounds=[0, 0.1],
+                bounds=[1e-6, 0.1],
                 transformation=pybop.LogTransformation(),
             ),
         )
@@ -70,8 +70,8 @@ class TestTheveninParameterisation:
     @pytest.mark.parametrize(
         "optimiser, method",
         [
-            (pybop.SciPyMinimize, "trust-constr"),
             (pybop.SciPyMinimize, "SLSQP"),
+            (pybop.SciPyMinimize, "trust-constr"),
             (pybop.SciPyMinimize, "L-BFGS-B"),
             (pybop.SciPyMinimize, "COBYLA"),
             (pybop.GradientDescent, ""),
@@ -106,7 +106,7 @@ class TestTheveninParameterisation:
 
         # Assertions
         if not np.allclose(x0, self.ground_truth, atol=1e-5):
-            if optim.minimising:
+            if results.minimising:
                 assert initial_cost > results.final_cost
             else:
                 assert initial_cost < results.final_cost

tests/unit/test_parameters.py
@@ -222,9 +222,7 @@ class TestParameters:
 
         # Test transformed bounds
         bounds = params.get_bounds(apply_transform=True)
-        np.testing.assert_allclose(
-            bounds["lower"], [np.log(np.finfo(float).eps), 0.5, 0, -1]
-        )
+        np.testing.assert_allclose(bounds["lower"], [-np.inf, 0.5, 0, -1])
         np.testing.assert_allclose(bounds["upper"], [np.log(1), 1.5, 1, 0])
 
         # Test unbounded transformation causes ValueError due to NaN