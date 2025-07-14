ff0ad49476a26acf0bb3a31303a085dc895fa26b
Dibyendu-IITKGP <32595915+Dibyendu-IITKGP@users.noreply.github.com>
2025-07-14 10:24:35 +0100

    Update papers/Hallemans et al/Fig13_EIS_Fitting_Simulation.py
    Co-authored-by: NicolaCourtier <45851982+NicolaCourtier@users.noreply.github.com>

--- papers/Hallemans et al/Fig13_EIS_Fitting_Simulation.py
+++ papers/Hallemans et al/Fig13_EIS_Fitting_Simulation.py
@@ -20,7 +20,7 @@ parameter_set["Electrolyte conductivity [S.m-1]"] = 1e16
 parameter_set["Negative electrode conductivity [S.m-1]"] = 1e16
 parameter_set["Positive electrode conductivity [S.m-1]"] = 1e16
 
-grouped_parameters = BaseGroupedSPMe.apply_parameter_grouping(parameter_set)
+grouped_parameters = pybop.lithium_ion.GroupedSPMe.apply_parameter_grouping(parameter_set)
 grouped_parameters["Series resistance [Ohm]"] = R0
 model_options = {"surface form": "differential", "contact resistance": "true"}
 var_pts = {"x_n": 100, "x_s": 20, "x_p": 100, "r_n": 100, "r_p": 100}
