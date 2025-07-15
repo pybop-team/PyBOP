fix: sampler starting positions, adds jacobian transformations

pybop/samplers/base_pints_sampler.py
@@ -181,7 +181,9 @@ class BasePintsSampler(BaseSampler):
                 self._end_initial_phase()
 
             xs = self._ask_for_samples()
-            self.fxs = evaluator.evaluate(xs)
+            model_xs = [self.problem.params.transformation().to_model(x) for x in xs]
+
+            self.fxs = evaluator.evaluate(model_xs)
             self._process_chains()
 
             if self._single_chain:
@@ -259,7 +261,10 @@ class BasePintsSampler(BaseSampler):
 
             def fun(x):
                 self.problem.set_params(x)
-                return self.problem.run_with_sensitivities()
+                loss, grad = self.problem.run_with_sensitivities()
+                jac = self.problem.params.transformation().jacobian(x)
+                grad = np.matmul(grad, jac)
+                return (-loss, -grad)
 
         else:
 

pybop/samplers/base_sampler.py
@@ -54,17 +54,9 @@ class BaseSampler:
         self.set_parallel(self.options.parallel)
 
         # Get initial conditions
-        if self._options.x0 is not None and np.isscalar(self._options.x0):
-            x0 = self._problem.params.transformation.to_search(
-                np.ones(self._options.n_chains) * self._options.x0
-            )
-        else:
-            x0 = problem.params.sample_from_priors(
-                n_samples=self._options.n_chains, transformed=True
-            )
-        if x0 is None:
-            raise ValueError("Initial parameter values could not be sampled.")
-        self._x0 = x0
+        self._x0 = self.problem.params.get_initial_values(transformed=True) * np.ones(
+            [self._options.n_chains, 1]
+        )
 
         param_dims = len(self.problem.params)
         if np.isscalar(self._options.cov):