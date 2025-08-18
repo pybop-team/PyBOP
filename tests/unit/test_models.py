80dc345a7c1e2e9f17c6f4d74631bf92bbdb9cfb
NicolaCourtier <45851982+NicolaCourtier@users.noreply.github.com>
2025-08-18 15:41:52 +0100

    Create generic test_models.py

---
+++ tests/unit/test_models.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pybamm
+import pytest
+
+import pybop
+
+class TestModels:
+    """
+    A class to test pybop created models.
+    """
+
+    pytestmark = pytest.mark.unit
+
+    @pytest.fixture(
+        params=[
+            pybop.ExponentialDecayModel,
+            pybop.lithium_ion.WeppnerHuggins,
+            pybop.lithium_ion.SPDiffusion,
+            pybop.lithium_ion.GroupedSPM,
+            pybop.lithium_ion.GroupedSPMe,
+        ],
+    )
+    def model(self, request):
+        return request.param
+
+    def test_model_creation(self, model):
+        model_instance = model()
+        assert model_instance is not None
+
+        parameter_values = model_instance.default_parameter_values
+        assert isinstance(parameter_values, pybamm.ParameterValues)
+
+        variable_list = model_instance.default_quick_plot_variables
+        assert isinstance(variable_list, list)
+
+    def test_model_simulation(self, model):
+        sim = pybamm.Simulation(model())
+        t_eval = np.linspace(0,10,11)
+        sol = sim.solve(t_eval=t_eval, t_interp=t_eval)
+        np.testing.assert_allclose(sol["Time [s]"].data, t_eval)
+
+        fig = sol.plot()
+        assert isinstance(fig, pybamm.QuickPlot)
