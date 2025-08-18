69af9cf4720b67afcd1204cf8f27a7f707913112
NicolaCourtier <45851982+NicolaCourtier@users.noreply.github.com>
2025-08-18 15:38:10 +0100

    Update grouped SPMe example

--- examples/scripts/models/grouped_SPMe.py
+++ examples/scripts/models/grouped_SPMe.py
@@ -3,11 +3,9 @@ import pybamm
 
 import pybop
 
-# This example introduces the Grouped SPMe model
-# which groups correlated parameters into a minimum
-# set for the Single Particle Model with Electrolyte (SPMe).
-# This allows for a minimal set of parameters
-# for identification purposes.
+# This example introduces the Grouped SPMe, a model which groups the correlated parameters
+# of the Single Particle Model with Electrolyte (SPMe) into a minimum set of parameters for
+# identification purposes.
 
 experiment = pybamm.Experiment(
     [
@@ -16,36 +14,42 @@ experiment = pybamm.Experiment(
     ],
 )
 
-# Set ParameterValues, the GroupedSPMe uses the Chen2020 as the basis
-parameter_values = pybop.lithium_ion.GroupedSPMe().default_parameter_values
-
-# To see the reduced parameter values, we can use to mapping method provided
-# by the GroupedSPMe class.
-parameter_set = pybamm.ParameterValues("Chen2020")
-grouped_parameters = pybop.lithium_ion.GroupedSPMe().create_grouped_parameters(
-    parameter_set
+# Use the Chen2020 parameters with a constant electrolyte conductivity
+param = pybamm.ParameterValues("Chen2020")
+ce0 = param["Initial concentration in electrolyte [mol.m-3]"]
+T = param["Initial temperature [K]"]
+param["Electrolyte conductivity [S.m-1]"] = param["Electrolyte conductivity [S.m-1]"](
+    ce0, T
 )
-print(grouped_parameters)
+param["Electrolyte diffusivity [m2.s-1]"] = param["Electrolyte diffusivity [m2.s-1]"](
+    ce0, T
+)
 
-# Solve the Grouped SPMe
+# Enable options
 model_options = {"surface form": "differential", "contact resistance": "true"}
-model = pybop.lithium_ion.GroupedSPMe(options=model_options)
-simulation = pybamm.Simulation(
-    model, parameter_values=parameter_values, experiment=experiment
-)
-sol = simulation.solve()
 
 # Solve the PyBaMM SPMe
-model_options = {"surface form": "differential", "contact resistance": "true"}
 model = pybamm.lithium_ion.SPMe(options=model_options)
-parameter_values = pybamm.ParameterValues("Chen2020")
+simulation = pybamm.Simulation(model, parameter_values=param, experiment=experiment)
+sol = simulation.solve()
+
+# To set the grouped parameters, we can use the mapping method provided
+# by the GroupedSPMe class
+grouped_param = pybop.lithium_ion.GroupedSPMe.create_grouped_parameters(
+    parameter_values=param
+)
+print(grouped_param)
+
+# Solve the Grouped SPMe
+model = pybop.lithium_ion.GroupedSPMe(options=model_options)
 simulation = pybamm.Simulation(
-    model, parameter_values=parameter_values, experiment=experiment
+    model, parameter_values=grouped_param, experiment=experiment
 )
 sol2 = simulation.solve()
 
 # Plot Models
 fig, ax = plt.subplots()
-ax.plot(sol.t, sol["Voltage [V]"].data)
-ax.plot(sol2.t, sol2["Voltage [V]"].data)
+ax.plot(sol.t, sol["Voltage [V]"].data, label="PyBaMM SPMe")
+ax.plot(sol2.t, sol2["Voltage [V]"].data, "--", label="Grouped SPMe")
+ax.legend()
 fig.show()
