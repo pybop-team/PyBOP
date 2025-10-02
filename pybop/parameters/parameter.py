26681cac44ea20f142e69fbaa29b6e46fa953ae5
NicolaCourtier <45851982+NicolaCourtier@users.noreply.github.com>
2025-10-02 21:53:39 +0100

    Update parameter.py

--- pybop/parameters/parameter.py
+++ pybop/parameters/parameter.py
@@ -272,7 +272,7 @@ class Parameter:
             return None
         return self._transformation.to_search(self._initial_value)[0]
 
-    def __call__(self, **unused) -> float:
+    def __call__(self, *unused_args, **unused_kwargs) -> float:
         "Return the current value. The unused arguments are to pass pybamm.ParameterValues checks."
         return self._current_value
 
