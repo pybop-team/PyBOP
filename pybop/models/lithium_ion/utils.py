14ae44431b233570170cfa47bbde5bcdb59d97e5
NicolaCourtier <45851982+NicolaCourtier@users.noreply.github.com>
2026-05-14 18:55:40 +0100

    Update utils

--- pybop/models/lithium_ion/utils.py
+++ pybop/models/lithium_ion/utils.py
@@ -3,6 +3,7 @@ from typing import TYPE_CHECKING
 
 import numpy as np
 from pybamm import Interpolant as PybammInterpolant
+from pybamm import Symbol
 from scipy import interpolate
 
 if TYPE_CHECKING:
@@ -51,7 +52,7 @@ class Interpolant:
     ):
         self.x = np.asarray(x)
         self.y = np.asarray(y)
-        self.name = name
+        self.name = name if name is None else name.replace(" ", "_")
         self.kind = kind or "linear"
         self._interp_func = self._create_interpolant(bounds_error, fill_value, axis)
 
@@ -82,14 +83,15 @@ class Interpolant:
         float, array_like, or pybamm.Interpolant
             Interpolated values or PyBaMM interpolant object.
         """
-        try:
-            # Try numeric evaluation first
-            return self._interp_func(x)
-        except Exception:
-            # Fall back to PyBaMM interpolant for symbolic evaluation
+        if isinstance(x, Symbol):  # symbolic evaluation
             return PybammInterpolant(
                 self.x, self.y, x, name=self.name, interpolator=self.kind
             )
+        else:  # numeric evaluation
+            return self._interp_func(x)
+
+    def __repr__(self):
+        return f"{self.name}, {self.kind} interpolant"
 
 
 class InverseOCV:
@@ -116,11 +118,13 @@ class InverseOCV:
         self.optimiser = optimiser or SciPyMinimize
         self.optimiser_options = optimiser_options or self.optimiser.default_options()
 
-        parameters = Parameters({"Root": Parameter(initial_value=0.5, bounds=[0, 1])})
+        self.parameters = Parameters(
+            {"Root": Parameter(initial_value=0.5, bounds=[0, 1])}
+        )
 
         # Set up a root-finding cost function
         class OCVRoot(BaseSimulator):
-            def __init__(self, ocv_value: float):
+            def __init__(self, parameters: Parameters, ocv_value: float):
                 super().__init__(parameters=parameters)
                 self.ocv_value = ocv_value
 
@@ -153,7 +157,7 @@ class InverseOCV:
         float
             The stoichiometry corresponding to the open-circuit voltage value.
         """
-        problem = Problem(self.ocv_root(ocv_value), self.cost)
+        problem = Problem(self.ocv_root(self.parameters, ocv_value), self.cost)
         optim = self.optimiser(problem, options=self.optimiser_options)
         result = optim.run()
         return result.best_inputs["Root"]
