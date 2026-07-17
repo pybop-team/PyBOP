ef2285c69b0a9ce0a37a6bf9a746b802332bf099
u2370093 <sarah.roggendorf@warwick.ac.uk>
2026-07-17 14:02:25 +0100

    update joss plotting scripts

--- papers/joss/design_plots.py
+++ papers/joss/design_plots.py
@@ -50,14 +50,14 @@ parameter_values.update(
     {
         "Positive electrode thickness [m]": pybop.Parameter(
             initial_value=8.88e-05,
-            prior=pybop.Gaussian(7.56e-05, 3e-05),
-            bounds=[50e-06, 120e-06],
+            distribution=pybop.Gaussian(
+                7.56e-05, 3e-05, truncated_at=[50e-06, 120e-06]
+            ),
             transformation=pybop.UnitHyperCube(lower=50e-6, upper=120e-6),
         ),
         "Positive electrode active material volume fraction": pybop.Parameter(
             initial_value=0.42,
-            prior=pybop.Gaussian(0.58, 0.1),
-            bounds=[0.3, 0.825],
+            distribution=pybop.Gaussian(0.58, 0.1, truncated_at=[0.3, 0.825]),
             transformation=pybop.UnitHyperCube(lower=0.3, upper=0.825),
         ),
     }
@@ -88,6 +88,8 @@ print(result)
 print("Estimated parameters:", result.x)
 print(f"Initial gravimetric energy density: {problem(result.x0):.1f} W.h.kg-1")
 print(f"Optimised gravimetric energy density: {problem(result.x):.1f} W.h.kg-1")
+initial_energy_density = problem(result.x0)
+optimised_energy_density = problem(result.x)
 
 if create_plot["gravimetric"]:
     # Plot the cost landscape with optimisation path
@@ -119,7 +121,7 @@ if create_plot["gravimetric"]:
 
 if create_plot["prediction"]:
     # Plot the timeseries output
-    problem.target = "Voltage [V]"
+    problem.set_target("Voltage [V]")
     figs = pybop.plot.problem(
         problem,
         inputs=result.best_inputs,
@@ -156,9 +158,11 @@ if create_plot["prediction"]:
     prediction_fig.data[1].update(line=dict(color="#00CC97"))
     prediction_fig.data[
         0
-    ].name = f"Initial: {problem(result.x0):.1f} W&#8239;h&#8239;kg<sup>-1</sup>"
+    ].name = f"Initial: {initial_energy_density:.1f} W&#8239;h&#8239;kg<sup>-1</sup>"
     prediction_fig.data[
         1
-    ].name = f"Optimised: {problem(result.x):.1f} W&#8239;h&#8239;kg<sup>-1</sup>"
+    ].name = (
+        f"Optimised: {optimised_energy_density:.1f} W&#8239;h&#8239;kg<sup>-1</sup>"
+    )
     prediction_fig.show()
     prediction_fig.write_image("figures/individual/design_prediction.pdf")
--- papers/joss/param_plots.py
+++ papers/joss/param_plots.py
@@ -101,7 +101,7 @@ if create_plot["simulation"]:
 dataset = pybop.Dataset(
     {
         "Time [s]": solution["Time [s]"].data,
-        "Current function [A]": solution["Current [A]"].data,
+        "Current [A]": solution["Current [A]"].data,
         "Voltage [V]": corrupt_values,
     }
 )
@@ -116,15 +116,13 @@ parameter_values.update(
     {
         "Contact resistance [Ohm]": pybop.Parameter(
             initial_value=initial_value[0],
-            prior=pybop.Gaussian(0.02, 0.005),
+            distribution=pybop.Gaussian(0.02, 0.005, truncated_at=[0.005, 0.025]),
             transformation=pybop.ScaledTransformation(coefficient=200),
-            bounds=[0.005, 0.025],
         ),
         "Negative particle diffusivity [m2.s-1]": pybop.Parameter(
             initial_value=initial_value[1],
-            prior=pybop.Gaussian(9e-14, 2e-14),
+            distribution=pybop.LogUniform(1.9e-14, 12e-14),
             transformation=pybop.LogTransformation(),
-            bounds=[1.9e-14, 12e-14],
         ),
     }
 )
@@ -293,22 +291,22 @@ if create_plot["maximising"]:
     first_MAP = True
 
     for cost in maximising_cost_classes:
+        # Define the cost and problem
         if cost is pybop.GaussianLogLikelihoodKnownSigma:
-            cost = cost(dataset, sigma0=sigma)
+            cost = cost(dataset, sigma=sigma)
+            problem = pybop.Problem(simulator, cost)
         elif cost is pybop.GaussianLogLikelihood:
-            cost = cost(dataset, sigma0=4 * sigma)
+            cost = cost(dataset, sigma=4 * sigma)
+            problem = pybop.Problem(simulator, cost)
         elif cost is pybop.LogPosterior and first_MAP:
-            cost = cost(
-                log_likelihood=pybop.GaussianLogLikelihoodKnownSigma(
-                    dataset, sigma0=sigma
-                )
-            )
+            cost = pybop.GaussianLogLikelihoodKnownSigma(dataset, sigma=sigma)
+            problem = pybop.LogPosterior(simulator, cost)
             first_MAP = False
         elif cost is pybop.LogPosterior:
-            cost = cost(log_likelihood=pybop.GaussianLogLikelihood(dataset))
+            cost = pybop.GaussianLogLikelihood(dataset)
+            problem = pybop.LogPosterior(simulator, cost)
 
-        # Define the problem and optimiser
-        problem = pybop.Problem(simulator, cost)
+        # Define the optimiser
         options = pybop.SciPyMinimizeOptions(maxiter=50, method="BFGS", jac=True)
         optim = pybop.SciPyMinimize(problem, options=options)
 
@@ -785,26 +783,44 @@ if create_plot["heuristic"]:
 if create_plot["posteriors"]:
     sigma0 = pybop.Parameter(
         initial_value=sigma,
-        prior=pybop.Uniform(1e-8 * sigma, 10 * sigma),
         bounds=[1e-8, 10 * sigma],
     )
-    likelihood = pybop.GaussianLogLikelihood(dataset, sigma0=sigma0)
-    posterior = pybop.Problem(simulator, pybop.LogPosterior(likelihood))
+    parameter_values2 = pybamm.ParameterValues("Chen2020")
+    parameter_values2["Contact resistance [Ohm]"] = 0.01
+    parameter_values2.update(
+        {
+            "Contact resistance [Ohm]": pybop.Parameter(
+                initial_value=initial_value[0],
+                distribution=pybop.Gaussian(0.02, 0.005, truncated_at=[0.005, 0.025]),
+                transformation=pybop.ScaledTransformation(coefficient=200),
+            ),
+            "Negative particle diffusivity [m2.s-1]": pybop.Parameter(
+                initial_value=initial_value[1],
+                distribution=pybop.Gaussian(
+                    9e-14, 2e-14, truncated_at=[1.9e-14, 12e-14]
+                ),
+            ),
+        }
+    )
+    simulator = pybop.pybamm.Simulator(
+        model, parameter_values=parameter_values2, protocol=dataset, solver=solver
+    )
+    likelihood = pybop.GaussianLogLikelihood(dataset, sigma=sigma0)
+    posterior = pybop.LogPosterior(simulator, likelihood)
 
     options = pybop.PintsSamplerOptions(
         n_chains=5,
         max_iterations=3500,
         warm_up_iterations=1500,
-        cov=posterior.parameters.get_sigma0(transformed=True),
     )
     sampler = pybop.HaarioBardenetACMC(posterior, options=options)
 
     result = sampler.run()
     print(result)
     print("True parameter values:", [true_value, sigma])
-    summary = pybop.PosteriorSummary(result.chains)
-    print(summary.rhat())
-    print(summary.effective_sample_size(mixed_chains=True))
+    summary = result.get_summary_statistics()
+    print(result.rhat())
+    print(result.effective_sample_size(mixed_chains=True))
 
     # Create a grid for subplots
     fig = plt.figure(figsize=(15, 6))
@@ -822,7 +838,7 @@ if create_plot["posteriors"]:
     # Subplot for parameter 0
     ax1 = fig.add_subplot(gs[0, 0])
     ax1.hist(
-        summary.all_samples[:, 0],
+        result.all_samples[:, 0],
         bins=50,
         density=False,
         alpha=0.6,
@@ -833,8 +849,8 @@ if create_plot["posteriors"]:
     ax1.set_ylim(0, 775)
     ax1.tick_params(axis="both", which="major", labelsize=tick_font_size)
     ax1.axvspan(
-        summary.get_summary_statistics()[("ci_lower")][0],
-        summary.get_summary_statistics()[("ci_upper")][0],
+        summary[("ci_lower")][0],
+        summary[("ci_upper")][0],
         alpha=0.1,
         color="tab:blue",
     )
@@ -844,7 +860,7 @@ if create_plot["posteriors"]:
     # Subplot for parameter 1
     ax2 = fig.add_subplot(gs[0, 1])
     ax2.hist(
-        summary.all_samples[:, 1],
+        result.all_samples[:, 1],
         bins=50,
         density=False,
         alpha=0.6,
@@ -855,8 +871,8 @@ if create_plot["posteriors"]:
     ax2.set_ylim(0, 775)
     ax2.tick_params(axis="both", which="major", labelsize=tick_font_size)
     ax2.axvspan(
-        summary.get_summary_statistics()[("ci_lower")][1],
-        summary.get_summary_statistics()[("ci_upper")][1],
+        summary[("ci_lower")][1],
+        summary[("ci_upper")][1],
         alpha=0.1,
         color="tab:red",
     )
@@ -866,7 +882,7 @@ if create_plot["posteriors"]:
     # Subplot for sigma
     ax3 = fig.add_subplot(gs[0, 2])
     ax3.hist(
-        summary.all_samples[:, 2],
+        result.all_samples[:, 2],
         bins=50,
         density=False,
         alpha=0.6,
@@ -876,8 +892,8 @@ if create_plot["posteriors"]:
     ax3.set_ylim(0, 775)
     ax3.tick_params(axis="both", which="major", labelsize=tick_font_size)
     ax3.axvspan(
-        summary.get_summary_statistics()[("ci_lower")][2],
-        summary.get_summary_statistics()[("ci_upper")][2],
+        summary[("ci_lower")][2],
+        summary[("ci_upper")][2],
         alpha=0.1,
         color="tab:purple",
     )
